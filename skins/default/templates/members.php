44f0f1daea035ac1505bf4201e4c0b52c5f2c6d6
theredfoxfire <vizzlearn@gmail.com>
2014-10-22 14:36:53 +0700

    5th-deploy

---
+++ skins/default/templates/members.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Powered by TinyWeb</title>
+	</head>
+	<body>
+	<h1>Our Members</h1>
+	<p>Belom is a list of our members:</p>
+	<ul>
+		<!-- START members -->
+		<li>{name} {email}</li>
+		<!-- END members -->
+	</ul>
+	</body>
+</html>
